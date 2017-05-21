fb4bd4615e31bb3cd8486f2150bbc71cc4315060
Daniel Merz <MerzDanielDeveloper@gmail.com>
2017-05-21 14:57:46 +0200

    logging and os.path.join in testing

--- src/Testing.py
+++ src/Testing.py
@@ -2,23 +2,30 @@ import os
 
 from VanishingPoint import *
 
+outputDir = os.path.join('..','pictures','output')
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+
 i = 0
 for subdir, dirs, files in os.walk('../pictures/input'):
     for file in files:
         filepath = subdir + os.sep + file
 
         if filepath.endswith(".jpg"):
+            print "Load Image" + filepath
             i += 1
             img = cv2.imread(filepath)
 
             hough_lines = hough_transform(img)
             if hough_lines:
+                print "--got hough_lines"
                 random_sample = sample_lines(hough_lines, 100)
                 intersections = find_intersections(random_sample, img)
                 if intersections:
+                    print "--got intersections"
                     grid_size = min(img.shape[0], img.shape[1]) // 3
                     vanishing_point = find_vanishing_point(img, grid_size, intersections)
 
-                    filename = '../pictures/output/center' + str(i) + '.jpg'
+                    filename = os.path.join(outputDir, 'center' + str(i) + '.jpg')
 
                     cv2.imwrite(filename, img)
